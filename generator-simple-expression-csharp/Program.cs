653ca79934e7ea950db51885122c95f857de2a25
Andrii Kurdiumov <kant2002@gmail.com>
2026-05-17 22:18:01 +0200

    Add sample of generator in C#

---
+++ generator-simple-expression-csharp/Program.cs
@@ -0,0 +1,89 @@
+﻿using dnlib.DotNet.Emit;
+
+var (expr, value) = Generator(3);
+var body = new CilBody();
+GenerateMethodBody(expr, body);
+
+Console.WriteLine("Expression");
+Console.WriteLine("{0}", expr);
+Console.WriteLine("Value");
+Console.WriteLine("{0}", value);
+
+Console.WriteLine("IL Code\n{0}", body);
+for (var i = 0; i < body.Instructions.Count; i++)
+{
+    Console.WriteLine("{0}", body.Instructions[i]);
+}
+
+(Expr, int) Generator(int fuel)
+{
+    if (fuel == 0)
+    {
+        var value = Random.Shared.Next(100);
+        return (new ConstInt32(value), value);
+    }
+    var op = Random.Shared.Next(6);
+    var (left, leftValue) = Generator(fuel - 1);
+    var (right, rightValue) = Generator(fuel - 1);
+    switch (op)
+    {
+        case 0:
+            return (new AddOperation(left, right), leftValue + rightValue);
+        case 1:
+            return (new SubOperation(left, right), leftValue - rightValue);
+        case 2:
+            return (new MulOperation(left, right), leftValue * rightValue);
+        case 3:
+            if (rightValue == 0)
+                return Generator(fuel - 1);
+            return (new DivOperation(left, right), leftValue / rightValue);
+        case 4:
+            if (rightValue == 0)
+                return Generator(fuel - 1);
+            return (new ModOperation(left, right), leftValue % rightValue);
+    }
+    return (new AddOperation(left, right), rightValue);
+}
+
+void GenerateMethodBody(Expr expr, CilBody il)
+{
+    switch (expr)
+    {
+        case ConstInt32 c:
+            il.Instructions.Add(Instruction.Create(OpCodes.Ldc_I4, c.Value));
+            break;
+        case AddOperation a:
+            GenerateMethodBody(a.Left, il);
+            GenerateMethodBody(a.Right, il);
+            il.Instructions.Add(Instruction.Create(OpCodes.Add));
+            break;
+        case SubOperation s:
+            GenerateMethodBody(s.Left, il);
+            GenerateMethodBody(s.Right, il);
+            il.Instructions.Add(Instruction.Create(OpCodes.Sub));
+            break;
+        case MulOperation m:
+            GenerateMethodBody(m.Left, il);
+            GenerateMethodBody(m.Right, il);
+            il.Instructions.Add(Instruction.Create(OpCodes.Mul));
+            break;
+        case DivOperation d:
+            GenerateMethodBody(d.Left, il);
+            GenerateMethodBody(d.Right, il);
+            il.Instructions.Add(Instruction.Create(OpCodes.Div));
+            break;
+        case ModOperation m:
+            GenerateMethodBody(m.Left, il);
+            GenerateMethodBody(m.Right, il);
+            il.Instructions.Add(Instruction.Create(OpCodes.Rem));
+            break;
+    }
+}
+
+abstract record Expr;
+record ConstInt32(int Value) : Expr;
+record AddOperation(Expr Left, Expr Right) : Expr;
+record SubOperation(Expr Left, Expr Right) : Expr;
+record MulOperation(Expr Left, Expr Right) : Expr;
+record DivOperation(Expr Left, Expr Right) : Expr;
+record ModOperation(Expr Left, Expr Right) : Expr;
